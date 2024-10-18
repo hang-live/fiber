2631ba326ba24e5587c34e286696e0c7bc52405d
Brandon Raphael <brandonraphael@Brandons-MacBook-Pro.local>
2024-10-18 12:08:05 -0700

    init

--- go.mod
+++ go.mod
@@ -4,4 +4,9 @@ module fiber
 
 go 1.16
 
-require github.com/gofiber/fiber/v2 v2.42.0
+require (
+	github.com/authorizerdev/authorizer-go v0.0.0-20240402105736-0eeef41a29a2 // indirect
+	github.com/gin-gonic/gin v1.10.0 // indirect
+	github.com/gofiber/fiber/v2 v2.42.0
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -2,29 +2,101 @@ package main
 
 import (
 	"os"
-
-	"github.com/gofiber/fiber/v2"
+	"net/http"
+	"strings"
+	"log"
+ 
+	"github.com/authorizerdev/authorizer-go"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-func getPort() string {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = ":3000"
-	} else {
-		port = ":" + port
-	}
+func AuthorizeMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		env := os.Getenv("ENV")
+		if env == "local" {
+			c.Next()
+			return
+		}
 
-	return port
+		/**
+		  for open routes you can add condition here and just return with c.Next()
+		  so that it does not validate token for those routes
+		*/
+		authHeader := c.Request.Header.Get("Authorization")
+		tokenSplit := strings.Split(authHeader, " ")
+ 
+		defaultHeaders := map[string]string{}
+		authorizerClient, err := authorizer.NewAuthorizerClient(os.Getenv("AUTHORIZER_CLIENT_ID"), os.Getenv("AUTHORIZER_URL"), os.Getenv("AUTHORIZER_REDIRECT_URL"), defaultHeaders)
+		if err != nil {
+			// unauthorized
+			c.AbortWithStatusJSON(401, "unauthorized")
+			return
+		}
+ 
+		if len(tokenSplit) < 2 || tokenSplit[1] == "" {
+			// unauthorized
+			c.AbortWithStatusJSON(401, "unauthorized")
+			return
+		}
+ 
+		res, err := authorizerClient.ValidateJWTToken(&authorizer.ValidateJWTTokenInput{
+			TokenType: authorizer.TokenTypeIDToken,
+			Token:     tokenSplit[1],
+		})
+		if err != nil {
+			// unauthorized
+			c.AbortWithStatusJSON(401, "unauthorized")
+			return
+		}
+ 
+		if !res.IsValid {
+			// unauthorized
+			c.AbortWithStatusJSON(401, "unauthorized")
+			return
+		}
+ 
+		c.Next()
+	}
 }
 
-func main() {
-	app := fiber.New()
+// func getPort() string {
+// 	port := os.Getenv("PORT")
+// 	if port == "" {
+// 		port = ":3000"
+// 	} else {
+// 		port = ":" + port
+// 	}
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
-			"message": "Hello, Railway!",
+// 	return port
+// }
+
+// func main() {
+// 	app := fiber.New()
+
+// 	app.Get("/", func(c *fiber.Ctx) error {
+// 		return c.JSON(fiber.Map{
+// 			"message": "Hello, Railway!",
+// 		})
+// 	})
+
+// 	app.Listen(getPort())
+// }
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+	  log.Fatal("Error loading .env file")
+	}
+
+	router := gin.New()
+	router.Use(AuthorizeMiddleware())
+ 
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
 		})
 	})
-
-	app.Listen(getPort())
+ 
+	router.Run(":8090")
 }
