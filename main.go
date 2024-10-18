3c119b01407dc8944de0c729b84ea9109a6401d2
Brandon Raphael <brandonraphael@Brandons-MacBook-Pro.local>
2024-10-18 15:50:21 -0700

    only origin

--- main.go
+++ main.go
@@ -93,8 +93,6 @@ func main() {
 	router.POST("/login", func(c *gin.Context) {
 		// Set CORS headers
 		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Methods", "POST, OPTIONS")
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		// Handle preflight OPTIONS request
 		if c.Request.Method == "OPTIONS" {
