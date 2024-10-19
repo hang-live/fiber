b93821394b6291bd153ae1d879c90633fc5b72e4
Brandon Raphael <brandonraphael@Brandons-MacBook-Pro.local>
2024-10-18 17:01:02 -0700

    allow origins

--- main.go
+++ main.go
@@ -133,7 +133,7 @@ func main() {
 		})
 		return
 	}).Use(cors.New(cors.Config{
-		AllowOrigins:     []string{os.Getenv("FRONTEND_URL")},
+		AllowOrigins:     []string{"http://localhost:3000", "https://hanglive.com"},
 		AllowMethods:     []string{http.MethodGet, http.MethodPatch, http.MethodPost, http.MethodHead, http.MethodDelete, http.MethodOptions},
 		AllowHeaders:     []string{"Content-Type", "X-XSRF-TOKEN", "Accept", "Origin", "X-Requested-With", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
